c93031362c72ab78aee20b3fdb02049664f65920
djordan <darren.jordan12@gmail.com>
2021-04-30 15:10:16 -0500

    add a shoppage and collection preview components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shoppage.component';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}></Route>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
