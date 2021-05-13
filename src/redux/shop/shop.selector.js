8c460dfadc42c2a04fa34c4fa222eb6b3640f59b
darren <darren.jordan12@gmail.com>
2021-05-13 11:57:04 -0500

    normalize shop data from array to hash table to optimize lookup of collection items

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -20,9 +12,7 @@ export const selectCollection = memoize((collectionUrlParam) => (
     // Curried Function, function that returns another function
     createSelector(
         [selectCollections],
-        collections => collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-            )
+        collections => collections[collectionUrlParam]
         )
     )
 )
